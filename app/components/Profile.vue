5e0a2c84517dd7682bdf172210b9448debb9cab5
Javari <javari@duck.com>
2025-11-06 17:04:58 +0330

    chore: Delete timeout in profile fetch

--- app/components/Profile.vue
+++ app/components/Profile.vue
@@ -8,10 +8,5 @@
 import Content from '~/components/ui/Content.vue'
 import ProfileContent from './profile/ProfileContent.vue'
 
-const { data: profile, pending, error } = await useFetch('/api/profile', {
-    server: false,
-    async onRequest({ options }) {
-        await new Promise(r => setTimeout(r, 1000))
-    }
-})
+const { data: profile, pending, error } = await useFetch('/api/profile', { server: false })
 </script>
